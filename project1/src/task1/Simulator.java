030cc77d200e7d70c5ad7af5d0e73e515882d2eb
Eivind Morch <eivinmor@stud.ntnu.no>
2017-01-22 18:34:05 +0100

    cleanup and moving functionality betweeen classes for better modularity

--- project1/src/task1/Simulator.java
+++ project1/src/task1/Simulator.java
@@ -1,30 +1,64 @@
 package task1;
 
-
 import java.util.Scanner;
-public class Simulator {
+
+
+class Simulator {
+
+    private Scanner sc;
+    private int trials;
+    private boolean stepByStep;
+
+    private Simulator(){
+        sc = new Scanner(System.in);
+        trials = 1;
+        stepByStep = true;
+    }
+
+    private void simulate(){
+        int sum = 0;
+        for (int i = 1; i <= trials; i++) {
+            int score = runTrial();
+            System.out.println("Trial " + i + " score: " + score);
+
+            sum += score;
+        }
+        System.out.println("---------------------\nTotal avg. score: " + sum/trials);
+    }
+
+    private int runTrial(){
+        World world = new World();
+        Agent agent = new Agent(world);
+        if (stepByStep) {
+            System.out.println("Initial world:");
+            printGrid(world.getGrid());
+            System.out.println();
+        }
+        int step = 0;
+        while(!world.simulationEnd && step < 50) {
+            if (stepByStep) {
+                sc.nextLine();
+                agent.step();
+                printGrid(world.getGrid());
+                System.out.println("Step " + step + " score: " + agent.getScore());
+            }
+            else agent.step();
+            step++;
+        }
+        return agent.getScore();
+    }
+
+    private void printGrid(char[][] grid){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int totalSum = 0;
-//        for (int i = 0; i < 1000; i++) {
-
-        World world = new World();
-        System.out.println("Initial world:");
-        world.printGrid();
-        Agent agent = new Agent(world);
-        System.out.println();
-        int iteration = 0;
-        while(!world.simulationEnd && iteration < 50) {
-            sc.nextLine();
-            agent.step();
-            world.printGrid();
-            iteration++;
-        }
-        System.out.println("Score: " + agent.getScore());
-
-//            totalSum += trialSum;
-//        }
-//        System.out.println("SIMULATION Avg score: " + totalSum/1000);
+        Simulator simulator = new Simulator();
+        simulator.simulate();
     }
 }
--- project1/src/task1/World.java
+++ project1/src/task1/World.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 
 class World {
-
     int n;
     private char[][] initialGrid, grid;
     private Random random;
-    public boolean simulationEnd;
+    boolean simulationEnd;
 
     World (){
         random = new Random();
@@ -30,16 +29,7 @@ class World {
         return newGrid;
     }
 
-    void printGrid(){
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                System.out.print(grid[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public char getSquareStatus (int y, int x){
+    char getSquareStatus (int y, int x){
         if (x >= n || x < 0 || y >= n || y < 0 )
             return 'W';
         return grid[y][x];
@@ -70,5 +60,9 @@ class World {
             return -100;
         }
         return 0;
+    }
+
+    char[][] getGrid(){
+        return grid;
     }
 }
