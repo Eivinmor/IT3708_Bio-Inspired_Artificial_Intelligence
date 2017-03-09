7bb4e6a98251e2fa506d4b8636372bf6eb535327
Eivind Morch <eivinmor@stud.ntnu.no>
2017-03-09 21:58:53 +0100

    TODOs

--- project3/src/main/java/ga/Algorithm.java
+++ project3/src/main/java/ga/Algorithm.java
@@ -3,6 +3,9 @@ package ga;
 
 public class Algorithm {
 
+    // TODO Mutation - tumour
+    // TODO Mutation - loop outside border, add if closer to this
+
     public Algorithm(){
 
     }
--- project3/src/main/java/ga/Segment.java
+++ project3/src/main/java/ga/Segment.java
@@ -1,5 +1,6 @@
 package ga;
 
 
+// TODO Sjekke om det er noen begrensninger med å bruke HashSet for Segment
 public class Segment {
 }
