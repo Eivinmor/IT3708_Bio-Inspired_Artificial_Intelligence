aac4542160085fcca56f7ad4713d888318568060
Eivind Morch <Eivind Morch>
2017-04-11 12:56:30 +0200

    Changed Particle and PSO. Solution needs change.

--- project4/src/main/java/pso/Particle.java
+++ project4/src/main/java/pso/Particle.java
@@ -16,43 +16,44 @@ public class Particle {
     Particle() {
         preferenceMatrix = new int[JSP.numOfMachines][JSP.numOfJobs];
         velocityMatrix = new int[JSP.numOfMachines][JSP.numOfJobs];
-        ArrayList<Integer> preferences = new ArrayList<>(JSP.numOfJobs);
-        for (int i = 0; i < JSP.numOfJobs; i++) preferences.add(i);
+        ArrayList<Integer> jobs = new ArrayList<>(JSP.numOfJobs);
+        for (int i = 0; i < JSP.numOfJobs; i++) jobs.add(i);
         for (int i = 0; i < JSP.numOfMachines; i++) {
-            Collections.shuffle(preferences);
-            for (int j = 0; j < preferences.size(); j++) preferenceMatrix[i][j] = preferences.get(j);
+            Collections.shuffle(jobs);
+            for (int j = 0; j < jobs.size(); j++) preferenceMatrix[i][j] = jobs.get(j);
         }
     }
 
     // TODO - Test
-    void moveToward(int machine, int job, Solution solution) {
-        int j1 = preferenceMatrix[machine][job];
+    void moveToward(int machine, int preference, Solution solution) {
+        int j1 = preferenceMatrix[machine][preference];
+
         int j1SolutionLocation = -1;
         for (int i = 0; i < JSP.numOfJobs; i++) {
             if (solution.schedule[machine][i] == j1) j1SolutionLocation = i;
         }
         int j2 = preferenceMatrix[machine][j1SolutionLocation];
 
-        if (velocityMatrix[machine][job] <= 0
-                && velocityMatrix[machine][j1SolutionLocation] <= 0
+        if (velocityMatrix[machine][j1] <= 0
+                && velocityMatrix[machine][j2] <= 0
                 && j1 != j2) {
-            preferenceMatrix[machine][job] = j2;
+            preferenceMatrix[machine][preference] = j2;
             preferenceMatrix[machine][j1SolutionLocation] = j1;
             // TODO Not change velocity of job2?
-            velocityMatrix[machine][job] = 1;
+            velocityMatrix[machine][j1] = 1;
         }
     }
 
     void mutate() {
         int machine = Tools.random.nextInt(JSP.numOfMachines);
-        int job1 = Tools.random.nextInt(JSP.numOfJobs);
-        int job2 = Tools.random.nextInt(JSP.numOfJobs);
-        int pref1 = preferenceMatrix[machine][job1];
-        int pref2 = preferenceMatrix[machine][job1];
-        preferenceMatrix[machine][job1] = pref2;
-        preferenceMatrix[machine][job2] = pref1;
-        velocityMatrix[machine][job1] = 1;
-        velocityMatrix[machine][job2] = 1;
+        int l1 = Tools.random.nextInt(JSP.numOfJobs);
+        int l2 = Tools.random.nextInt(JSP.numOfJobs);
+        int j1 = preferenceMatrix[machine][l1];
+        int j2 = preferenceMatrix[machine][l2];
+        preferenceMatrix[machine][l1] = j2;
+        preferenceMatrix[machine][l2] = j1;
+        velocityMatrix[machine][j1] = 1;
+        velocityMatrix[machine][j2] = 1;
     }
 
 
