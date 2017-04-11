c7e6a43175bd61f48696068db1e0c30e144c19a9
Eivind Morch <eivindmor@stud.ntnu.no>
2017-04-11 13:12:06 +0200

    Cleanup in Particle

--- project4/src/main/java/pso/Particle.java
+++ project4/src/main/java/pso/Particle.java
@@ -16,17 +16,17 @@ public class Particle {
     Particle() {
         preferenceMatrix = new int[JSP.numOfMachines][JSP.numOfJobs];
         velocityMatrix = new int[JSP.numOfMachines][JSP.numOfJobs];
-        ArrayList<Integer> jobs = new ArrayList<>(JSP.numOfJobs);
-        for (int i = 0; i < JSP.numOfJobs; i++) jobs.add(i);
+        ArrayList<Integer> jobsIds = new ArrayList<>(JSP.numOfJobs);
+        for (int i = 0; i < JSP.numOfJobs; i++) jobsIds.add(i);
         for (int i = 0; i < JSP.numOfMachines; i++) {
-            Collections.shuffle(jobs);
-            for (int j = 0; j < jobs.size(); j++) preferenceMatrix[i][j] = jobs.get(j);
+            Collections.shuffle(jobsIds);
+            for (int j = 0; j < jobsIds.size(); j++) preferenceMatrix[i][j] = jobsIds.get(j);
         }
     }
 
     // TODO - Test
-    void moveToward(int machine, int preference, Solution solution) {
-        int j1 = preferenceMatrix[machine][preference];
+    void moveToward(int machine, int l, Solution solution) {
+        int j1 = preferenceMatrix[machine][l];
 
         int j1SolutionLocation = -1;
         for (int i = 0; i < JSP.numOfJobs; i++) {
@@ -34,10 +34,8 @@ public class Particle {
         }
         int j2 = preferenceMatrix[machine][j1SolutionLocation];
 
-        if (velocityMatrix[machine][j1] <= 0
-                && velocityMatrix[machine][j2] <= 0
-                && j1 != j2) {
-            preferenceMatrix[machine][preference] = j2;
+        if (velocityMatrix[machine][j1] == 0 && velocityMatrix[machine][j2] == 0 && j1 != j2) {
+            preferenceMatrix[machine][l] = j2;
             preferenceMatrix[machine][j1SolutionLocation] = j1;
             // TODO Not change velocity of job2?
             velocityMatrix[machine][j1] = 1;
@@ -56,63 +54,5 @@ public class Particle {
         velocityMatrix[machine][j2] = 1;
     }
 
-
-//    public int[][] generateSchedule() {
-//        // TODO Kan optimaliseres uten HashSet
-//
-//        int[][] schedule = new int[JSP.numOfMachines][JSP.numOfJobs];
-//        ArrayList<Operation> schedulable = new ArrayList<>(JSP.numOfMachines);
-//        double[] jobEndTime = new double[JSP.numOfJobs];
-//        double[] machineEndTime = new double[JSP.numOfMachines];
-//        int[] machineOperations = new int[JSP.numOfMachines];
-//
-//        @SuppressWarnings("unchecked")
-//        ArrayList<Operation>[] jobQueues = new ArrayList[JSP.numOfJobs];
-//        for (int i = 0; i < jobQueues.length; i++) {
-//            jobQueues[i] = new ArrayList<>(Arrays.asList(JSP.jobs[i]));
-//            schedulable.add(jobQueues[i].remove(0));
-//        }
-//        while (!schedulable.isEmpty()) {
-//            // Find best machine
-//            Operation earliestDoneOperation = schedulable.get(0);
-//            double bestFinishTime = jobEndTime[earliestDoneOperation.job] + earliestDoneOperation.duration;
-//            for (int i = 1; i < schedulable.size(); i++) {
-//                Operation op = schedulable.get(i);
-//                double opFinisTime = Math.max(jobEndTime[op.job], machineEndTime[op.machine]) + op.duration;
-//                if (opFinisTime < bestFinishTime) {
-//                    earliestDoneOperation = op;
-//                    bestFinishTime = opFinisTime;
-//                }
-//            }
-//            // Find highest prio within machine of best
-//            int bestMachine = earliestDoneOperation.machine;
-//            Operation prioOp = earliestDoneOperation;
-//            for (Operation op : schedulable) {
-//                if (op.machine == bestMachine
-//                        && preferenceMatrix[op.machine][op.job] < preferenceMatrix[prioOp.machine][prioOp.job])
-//                    prioOp = op;
-//            }
-//            double prioOpFinishTime = Math.max(jobEndTime[prioOp.job], machineEndTime[prioOp.machine]) + prioOp.duration;
-//            schedulable.remove(prioOp);
-//            if (!jobQueues[prioOp.job].isEmpty()) schedulable.add(jobQueues[prioOp.job].remove(0));
-//            schedule[prioOp.machine][prioOp.job] = machineOperations[prioOp.machine];
-//            machineOperations[prioOp.machine]++;
-//            jobEndTime[prioOp.job] = prioOpFinishTime;
-//            machineEndTime[prioOp.machine] = prioOpFinishTime;
-//            int counter = 0;
-//            for (Operation op : JSP.jobs[prioOp.job]) {
-//                if (op.machine == prioOp.machine)
-//                    operationStartTimes[prioOp.job][counter] = prioOpFinishTime - prioOp.duration;
-//                counter++;
-//            }
-////            operationStartTimes[prioOp.job][prioOp.machine] = prioOpFinishTime - prioOp.duration;
-////            System.out.println(prioOp);
-//        }
-//        double maxSpan = machineEndTime[0];
-//        for (int i = 1; i < machineEndTime.length; i++)
-//            if (machineEndTime[i] > maxSpan) maxSpan = machineEndTime[i];
-//        makespan = maxSpan;
-//        return schedule;
-//    }
 }
 
