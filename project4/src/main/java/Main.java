a00c846621e068005296769e9dcddcacd7d9d3fd
Eivind Morch <eivinmor@stud.ntnu.no>
2017-04-23 12:43:54 +0200

    Added switch to main

--- project4/src/main/java/Main.java
+++ project4/src/main/java/Main.java
@@ -5,55 +5,21 @@ import utility.DataReader;
 
 public class Main {
 
+    private enum Algorithm {PSO, ACO, BCO}
+    private static Algorithm algorithm = Algorithm.PSO;
+
     public static void main(String[] args) {
         String[] strAr = DataReader.readOdtToStringArray(Settings.fileId);
         DataReader.makeRepresentation(strAr);
-        PSO pso = new PSO();
-        pso.runAlgorithm();
+        switch (algorithm) {
+            case PSO:
+                PSO pso = new PSO();
+                pso.runAlgorithm();
+                break;
+            case ACO:
+                break;
+            case BCO:
+                break;
+        }
     }
-
-//    public static void main(String[] args) {
-//        String[] strAr = DataReader.readOdtToStringArray(1);
-//        DataReader.makeRepresentation(strAr);
-//
-//        for (int i = 0; i < JSP.numOfJobs; i++) {
-//            System.out.println(Arrays.toString(JSP.jobs[i]));
-//        }
-//
-//        Particle p = new Particle();
-//        for (int i = 0; i < p.preferenceMatrix.length; i++) {
-//            System.out.println(Arrays.toString(p.preferenceMatrix[i]));
-//        }
-//        System.out.println();
-//
-//        int[][] schedule = p.generateSchedule();
-//        for (int i = 0; i < schedule.length; i++) {
-//            System.out.println(Arrays.toString(schedule[i]));
-//        }
-//
-//        System.out.println("\nOperation start times:");
-//        for (int i = 0; i < p.operationStartTimes.length; i++) {
-//            System.out.println(Arrays.toString(p.operationStartTimes[i]));
-//        }
-//
-//        Plotter plotter = new Plotter();
-//        plotter.plotPSOSchedule(p);
-//
-//        double minMakespan = Double.MAX_VALUE;
-//        while (true) {
-//            try {
-//                TimeUnit.MILLISECONDS.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            Particle p2 = new Particle();
-//            p2.generateSchedule();
-//            plotter.plotPSOSchedule(p2);
-//            if (p2.makespan<minMakespan) {
-//                minMakespan = p2.makespan;
-//                System.out.println(minMakespan);
-//            }
-//        }
-//    }
-
 }
