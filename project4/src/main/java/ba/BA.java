1aefce5beee6fb6af40c7676bab902ace3a50840
Eivind Morch <eivinmor@stud.ntnu.no>
2017-04-29 12:52:00 +0200

    Added constructInitialSolutions() to BA.

--- project4/src/main/java/ba/BA.java
+++ project4/src/main/java/ba/BA.java
@@ -4,20 +4,42 @@ import representation.JSP;
 import utility.Tools;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class BA {
 
     public void runAlgorithm() {
+//
+//        JSP.numOfJobs = 3;
+//        JSP.numOfMachines = 3;
+//        JSP.numOfOperations = 9;
+//
+//        int[] s1 = {1, 2, 0, 2, 0, 1, 0, 1, 2};
+//        int[] s2 = {0, 1, 2, 0, 2, 1, 2, 1, 0};
 
-        JSP.numOfJobs = 3;
-        JSP.numOfMachines = 3;
-        JSP.numOfOperations = 9;
+//        crossover(s1, s2);
 
-        int[] s1 = {1, 2, 0, 2, 0, 1, 0, 1, 2};
-        int[] s2 = {0, 1, 2, 0, 2, 1, 2, 1, 0};
+        constructInitialSolutions();
+        for (int i = 0; i < Settings.rounds; i++) {
+            
+        }
+    }
 
-        crossover(s1, s2);
+    private int[][] constructInitialSolutions() {
+        ArrayList<Integer> jobs = new ArrayList<>(JSP.numOfOperations);
+        for (int i = 0; i < JSP.numOfJobs; i++) {
+            for (int j = 0; j < JSP.numOfMachines; j++) jobs.add(i);
+        }
+        int[][] solutions = new int[Settings.employed][JSP.numOfOperations];
+        for (int i = 0; i < Settings.employed; i++) {
+            int[] solution = new int[JSP.numOfOperations];
+            Collections.shuffle(jobs);
+            for (int j = 0; j < JSP.numOfOperations; j++) {
+                solution[j] = jobs.get(j);
+            }
+            solutions[i] = solution;
+        }
+        return solutions;
     }
 
     private int[] crossover(int[] p1, int[] p2) {
--- project4/src/main/java/ba/Settings.java
+++ project4/src/main/java/ba/Settings.java
@@ -2,4 +2,10 @@ package ba;
 
 public class Settings {
 
+    public static final int rounds = 200;
+
+    public static final int employed = 25;
+    public static final int onlookers = 25;
+
+
 }
