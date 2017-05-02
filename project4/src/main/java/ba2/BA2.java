7f41f31c8ce698ebd38993a6492e8a35adfa9726
Eivind Morch <eivindmor@stud.ntnu.no>
2017-05-02 16:48:37 +0200

    BA2 Works!

--- project4/src/main/java/ba2/BA2.java
+++ project4/src/main/java/ba2/BA2.java
@@ -5,25 +5,27 @@ import representation.JSP;
 import utility.Tools;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 public class BA2 {
-
-    double neighbourhoodSize = Settings.initialNeighbourhoodSize;
 
     public void runAlgorithm() {
 
         // Initiate scouted solutions (generate random solutions)
-        ArrayList<BA2Solution> population = scatterScouts(Settings.populationSize);
+        ArrayList<BA2Solution> population = scatterScouts(Settings.numOfScoutBees);
         ArrayList<BA2Solution> nextPopulation;
         BA2Solution bestSolution = population.get(0);
 
         // START LOOP
         while (true) {
-            // Sort scouted sites (generate random solutions)
-            Collections.sort(population);
             nextPopulation = new ArrayList<>(Settings.populationSize);
-            neighbourhoodSize *= (1 - Settings.neighbourhoodReduction);
+
+            // Sort scouted sites
+            Collections.sort(population);
+
+            // Replace best solution
             if (population.get(0).makespan < bestSolution.makespan) bestSolution = population.get(0);
 
             // Present nb best of the scouted sites
@@ -35,10 +37,11 @@ public class BA2 {
                 // Best search
                 nextPopulation.add(searchNeighbourhood(population.get(i), Settings.beesPerBestSite));
             }
+
             // Site abandonment
             ArrayList<BA2Solution> nextPopulationAfterAbandonment = new ArrayList<>(Settings.populationSize);
             for (BA2Solution site : nextPopulation) {
-                if (site.roundsWithoutImprovement >= Settings.numOfStagnationRoundsBeforeAbandonment) {
+                if (++site.roundsWithoutImprovement <= Settings.numOfStagnationRoundsBeforeAbandonment) {
                    nextPopulationAfterAbandonment.add(site);
                 }
             }
@@ -48,9 +51,19 @@ public class BA2 {
             nextPopulation.addAll
                     (scatterScouts(Settings.populationSize - nextPopulation.size()));
 
-            population = nextPopulation;
+            population = new ArrayList<>(nextPopulation);
             System.out.println(bestSolution.makespan);
-            Tools.plotter.plotSolution(bestSolution);
+
+//            System.out.println();
+//            for (BA2Solution s : population) {
+//                System.out.print(s.neighbourhoodSize + " ");
+//            }
+//            System.out.println();
+//            try {
+//                TimeUnit.SECONDS.sleep(2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
@@ -60,7 +73,7 @@ public class BA2 {
             for (int j = 0; j < JSP.numOfMachines; j++) jobs.add(i);
         }
         ArrayList<BA2Solution> solutions = new ArrayList<>(numberOfScouts);
-        for (int i = 0; i < Settings.numOfScoutBees; i++) {
+        for (int i = 0; i < numberOfScouts; i++) {
             int[] foodSource = new int[JSP.numOfOperations];
             Collections.shuffle(jobs);
             for (int j = 0; j < JSP.numOfOperations; j++) {
@@ -72,12 +85,39 @@ public class BA2 {
     }
 
     private BA2Solution searchNeighbourhood(BA2Solution solution, int numOfBees) {
-        // Shrink neighbourhoods
-        BA2Solution bestNeighbourhoodSolution = null;
+        boolean changed = false;
+        BA2Solution bestNeighbourhoodSolution = solution;
         for (int i = 0; i < numOfBees; i++) {
-
+            BA2Solution neighbourSolution = getNeighbourSolution(solution);
+            if (neighbourSolution.makespan <= bestNeighbourhoodSolution.makespan) {
+                bestNeighbourhoodSolution = neighbourSolution;
+                changed = true;
+            }
         }
-        return solution;
+        if (changed) bestNeighbourhoodSolution.neighbourhoodSize = solution.neighbourhoodSize;
+        else {
+            bestNeighbourhoodSolution.neighbourhoodSize *= (1 - Settings.neighbourhoodReduction);
+        }
+//        System.out.println(bestNeighbourhoodSolution.neighbourhoodSize);
+        return bestNeighbourhoodSolution;
+    }
+
+    private BA2Solution getNeighbourSolution(BA2Solution solution) {
+        int[] newFoodSource = new int[JSP.numOfOperations];
+        ArrayList<Integer> selectedIndexes = new ArrayList<>(JSP.numOfOperations);
+        for (int i = 0; i < JSP.numOfOperations; i++) {
+            if (Tools.random.nextDouble() <= solution.neighbourhoodSize) {
+                selectedIndexes.add(i);
+            }
+            else newFoodSource[i] = solution.foodSource[i];
+        }
+        ArrayList<Integer> shuffledSelected = new ArrayList<>(selectedIndexes);
+        Collections.shuffle(shuffledSelected);
+
+        for (int i = 0; i < selectedIndexes.size(); i++) {
+            newFoodSource[selectedIndexes.get(i)] = solution.foodSource[shuffledSelected.get(i)];
+        }
+        return new BA2Solution(newFoodSource);
     }
 
 }
--- project4/src/main/java/ba2/Settings.java
+++ project4/src/main/java/ba2/Settings.java
@@ -5,14 +5,14 @@ class Settings {
     static final int populationSize = 200;       // n
     static final int numOfScoutBees = 100;      // ns
 
-    static final int numOfEliteSites = 5;       // ne
-    static final int numOfBestSites = 20;       // nb
+    static final int numOfEliteSites = populationSize/10;           // ne
+    static final int numOfBestSites = populationSize/3;            // nb
 
-    static final int beesPerEliteSite = 5;      // nre
-    static final int beesPerBestSite = 2;       // nrb
+    static final int beesPerEliteSite = 50;      // nre
+    static final int beesPerBestSite = 20;       // nrb
 
     static final double initialNeighbourhoodSize = 1;                  // ngh
-    static final double neighbourhoodReduction = 0.001;
+    static final double neighbourhoodReduction = 0.05;
 
     static final int numOfStagnationRoundsBeforeAbandonment = 5;        // stlim
 
